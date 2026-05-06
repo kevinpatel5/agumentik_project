4a798609556d932d6ba6cca40160b6580e53278b
Kevin Patel <mrunknown5180@gmail.com>
2026-05-06 09:24:35 +0530

    Fix CORS and auth issues

--- frontend/src/redux/slices/taskSlice.js
+++ frontend/src/redux/slices/taskSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = import.meta.env.VITE_API_URL +  '/tasks';
 
 const getToken = () => localStorage.getItem('token');
 
